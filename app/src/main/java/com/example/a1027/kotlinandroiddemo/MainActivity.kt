8366d4c0b75eebf009a389907c185fef1bda236d
Me <10jade0070@gmail.com>
2018-07-12 18:17:08 +0900

    Android 버튼 - OnClickListener

--- app/src/main/java/com/example/a1027/kotlinandroiddemo/MainActivity.kt
+++ app/src/main/java/com/example/a1027/kotlinandroiddemo/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,17 +21,13 @@ class MainActivity : AppCompatActivity() {
 //            // 사용자가 TextView를 클릭 할 때 수행 할 코드
 //            Toast.makeText(this@MainActivity, "You clicked on TextView 'Click Me'.", Toast.LENGTH_SHORT).show()
 
-        val ll_main = findViewById(R.id.ll_main_layout) as LinearLayout
-
-        // 버튼 만들기
-        val button_dynamic = Button(this)
-        // 레이아웃 매개 변수를 사용하여 layout_width 및 layout_height 설정
-        button_dynamic.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-        button_dynamic.text = "Dynamic Button"
-        // LinearLayout에 Button을 추가한다.
-        ll_main.addView(button_dynamic)
-
+             // 버튼에 대한 참조 가져 오기
+             val btn_click_me = findViewById(R.id.btn_click_me) as Button
+             // 클릭하여 listener를 설정합니다.
+             btn_click_me.setOnClickListener {
+            // 사용자가 버튼을 클릭 할 때 수행 할 코드
+            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
         }
     }
-
+}
 
